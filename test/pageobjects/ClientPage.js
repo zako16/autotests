6b8922e61b9e407dcdd5c73bb556f42089207e79
zako16 <meerim.zhanuzakova@kandasoft.com>
2023-01-10 10:52:01 +0600

    continue add client spec

--- test/pageobjects/ClientPage.js
+++ test/pageobjects/ClientPage.js
@@ -5,9 +5,38 @@ class ClientPage {
     get surnameField() { return $('input[formcontrolname="userSurname"]')}
     get firstnameField() { return $('input[formcontrolname="userName"]')}
 
+    get genderRadio() { return $('mat-radio-group mat-radio-button div.mat-radio-label-content')}
+
+    get emailField() { return $('input[type="email"]')}
+    get phoneField() { return $('input[formcontrolname="phone"]')}
+    get birthField() { return $('input[formcontrolname="birthday"]')}
+    get middleNameField() { return $('input[formcontrolname="userMiddleName"]')}
+    get saveButton() { return $('button[name="save"]')}
+
+
+    get openChildModalPageButton() { return $('div.add-user-children span i')}
+    get childsModalPage() { return $('mat-dialog-container app-children-dialog')}
+    get childsModalPageAddChildOption() { return this.childsModalPage.$('button.mat-raised-button')}
+    get childEnterRow() { return this.childsModalPage.$('div.crm-row')}
+    get childNameField() { return this.childEnterRow.$('input[formcontrolname="Name"]')}
+    get childBirthDateField() { return this.childEnterRow.$('input[formcontrolname="Birthday"]')}
+    get childGenderSelect() { return this.childEnterRow.$('mat-select')}
+    get childGenderSelectOption() { return $('//mat-option/span[contains(text(),"М")]')}
+    get addChild() { return this.childsModalPage.$('//mat-dialog-actions/button[contains(text(),"Сохранить")]')}
+
     // второй способ
     async fillForm(testingUser){
         await this.surnameField.setValue(testingUser.surname)
+    }
+
+    async fillChild(childName="Phillip", birthdate="05/06/2000"){
+        await this.openChildModalPageButton.click();
+        await this.childsModalPageAddChildOption.click();
+        await this.childNameField.setValue(childName);
+        await this.childBirthDateField.setValue(birthdate)
+        await this.childGenderSelect.click();
+        await this.childGenderSelectOption.click();
+        await this.addChild.click();
     }
 
     async open(){
--- test/specs/clients.spec.js
+++ test/specs/clients.spec.js
@@ -5,27 +5,41 @@ describe('Clients test', () => {
     beforeEach( async() => {
         await LoginPage.open();
         await LoginPage.doLogin();
-        await ClientPage.open();
+        // await ClientPage.open();
+    })
+    afterEach('Clear browser', async()=>{
+        await browser.reloadSession();
     })
 
     it('should create new client', async ()=> {
         // уже тут шаги открытия формы клиента и заполнения 
        // первый способ
+        // await browser.pause(5000);
+        // implicit , explicit
+        await ClientPage.createClientButton.waitForDisplayed({ timeout: 10000 });
         await ClientPage.createClientButton.click();
         await ClientPage.surnameField.setValue("Тестовая фамилия")
         await ClientPage.firstnameField.setValue("Тестовое имя")
+        await ClientPage.genderRadio.click();
+        await ClientPage.emailField.setValue("bellatrix.black.tester06@gmail.com");
+        await ClientPage.middleNameField.setValue("Отчество");
+        await ClientPage.birthField.setValue("05/06/1990");
+        await ClientPage.phoneField.setValue("996700555666");
+
+        await ClientPage.fillChild();
+        await ClientPage.saveButton.click();
+        await browser.acceptAlert();
+
 
         //  второй способ
-        let testingUser = {
-            surname: "Тестовая фамилия"
-        }
-        await ClientPage.fillForm(testingUser)
+        // let testingUser = {
+        //     surname: "Тестовая фамилия"
+        // }
+        // await ClientPage.fillForm(testingUser)
         // окончание второго способ
-
-        await browser.pause(5000);
     })
 
-    it('remain test cases', async () => {
+    xit('remain test cases', async () => {
 
     })
 
